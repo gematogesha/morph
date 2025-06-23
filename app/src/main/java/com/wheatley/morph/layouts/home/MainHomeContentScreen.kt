6e1f1a7f2f835e3d5dec163d93e1cfa8ec22c1c9
Alan Wheatley <mr.rubikscube@mail.ru>
2025-06-24 04:12:15 +0500

    Вёрстка: Улучшение отступов и обработки WindowInsets в MainHomeContentScreen
    Этот коммит вносит следующие изменения в `MainHomeContentScreen.kt`:
    
    - Добавлен `windowInsetsPadding` к `Scaffold` для учёта горизонтальных отступов навигационной панели.
    - Увеличен верхний отступ (`padding(top = 8.dp)`) для `LazyColumn`, содержащего карточки расписания, для лучшего визуального разделения.
    - Уменьшен нижний отступ (`padding(bottom = 56.dp)`) для `CardBig` в нижней части экрана.

--- app/src/main/java/com/wheatley/morph/layouts/home/MainHomeContentScreen.kt
+++ app/src/main/java/com/wheatley/morph/layouts/home/MainHomeContentScreen.kt
@@ -11,12 +11,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
@@ -105,7 +107,9 @@ fun MainHomeContentScreen(
     var selectedDate by remember { mutableStateOf<LocalDate?>(null) }
 
     Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        modifier = Modifier
+            .nestedScroll(scrollBehavior.nestedScrollConnection)
+            .windowInsetsPadding(WindowInsets.navigationBars.only(WindowInsetsSides.Horizontal)),
         topBar = {
             TopAppBar(
                 title = {
@@ -144,6 +148,7 @@ fun MainHomeContentScreen(
                     contentPadding = innerPadding,
                     modifier = Modifier
                         .fillMaxSize()
+                        .padding(top = 8.dp)
                         .padding(horizontal = 16.dp)
                 ) {
 
@@ -239,7 +244,7 @@ fun CalendarGrid(
 
     CardBig(
         modifier = Modifier
-            .padding(bottom = 64.dp)
+            .padding(bottom = 56.dp)
             .windowInsetsPadding(WindowInsets.navigationBars)
     ) {
         Column() {
