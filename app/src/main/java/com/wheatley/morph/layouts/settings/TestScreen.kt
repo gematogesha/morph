dd48f6346ac1707fc13ddb6e99dad9c1af493421
Ivan <mr.rubikscube@mail.ru>
2025-06-25 16:18:21 +0500

    Создан тестовый Sreen

---
+++ app/src/main/java/com/wheatley/morph/layouts/settings/TestScreen.kt
@@ -0,0 +1,13 @@
+package com.wheatley.morph.layouts.settings
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+class TestScreen: Screen {
+
+    @Composable
+    override fun Content() {
+        TODO("Not yet implemented")
+    }
+
+}
