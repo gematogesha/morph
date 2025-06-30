45c8307c0bcf7e22c8798537539a1a4ffcae66c0
Alan Wheatley <mr.rubikscube@mail.ru>
2025-07-01 03:01:54 +0500

    Реализация AppNavHost под Voyadger

--- app/src/main/java/com/wheatley/morph/presentation/MainActivity.kt
+++ app/src/main/java/com/wheatley/morph/presentation/MainActivity.kt
@@ -1,56 +1,49 @@
 package com.wheatley.morph.presentation
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.compose.rememberNavController
-import com.wheatley.morph.di.initKoinModules
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.Navigator
 import com.wheatley.morph.model.user.UserPrefs
+import com.wheatley.morph.presentation.onboarding.OnBoardingScreen
 import com.wheatley.morph.ui.theme.ApplySystemUi
 import com.wheatley.morph.ui.theme.MorphTheme
 import com.wheatley.morph.util.ui.ThemeManager
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        initKoinModules(this)
         ThemeManager.loadTheme(this)
         enableEdgeToEdge()
-        super.onCreate(savedInstanceState)
 
+        var isLoading = true
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { isLoading }
+
+        super.onCreate(savedInstanceState)
 
         setContent {
             val context = applicationContext
-            val navController = rememberNavController()
-            var startDestination by remember { mutableStateOf<String?>(null) }
+            var startScreen: Screen? by remember { mutableStateOf<Screen?>(null) }
 
             MorphTheme {
-                
                 ApplySystemUi()
-
-                actionBar?.hide()
-
                 Surface {
-                    if (startDestination != null) {
-                        AppNavHost(
-                            navController = navController,
-                            startDestination = startDestination!!
-                        )
+                    if (startScreen != null) {
+                        Navigator(screen = startScreen!!)
                     }
                 }
             }
 
             LaunchedEffect(Unit) {
                 val isRegistered = UserPrefs.isRegistered(context)
-                startDestination = if (isRegistered) "dashboard" else "onboarding"
+                startScreen = if (isRegistered) DashboardScreen() else OnBoardingScreen()
+                isLoading = false
             }
         }
     }
