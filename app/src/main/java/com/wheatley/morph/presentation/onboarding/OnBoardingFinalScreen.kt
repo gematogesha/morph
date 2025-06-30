379795e8f6fc4cd4f3e3c1069ab904f671f17bf7
Ivan <mr.rubikscube@mail.ru>
2025-06-30 16:52:07 +0500

    Реализация OnBoarding под Voyadger

---
+++ app/src/main/java/com/wheatley/morph/presentation/onboarding/OnBoardingFinalScreen.kt
@@ -0,0 +1,35 @@
+package com.wheatley.morph.presentation.onboarding
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.core.screen.Screen
+import com.wheatley.morph.presentation.components.PrimaryButton
+import com.wheatley.morph.presentation.onboarding.model.OnBoardingScreenModel
+
+class OnBoardingFinalScreen(
+    private val screenModel: OnBoardingScreenModel
+): Screen {
+    @Composable
+    override fun Content() {
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                PrimaryButton(
+                    onClick = { screenModel.exit() },
+                    text = "Завершить"
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/wheatley/morph/presentation/onboarding/OnBoardingNameScreen.kt
@@ -0,0 +1,98 @@
+package com.wheatley.morph.presentation.onboarding
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import com.wheatley.morph.presentation.components.CustomTextField
+import com.wheatley.morph.presentation.components.PrimaryButton
+import com.wheatley.morph.presentation.onboarding.model.OnBoardingScreenModel
+
+class OnBoardingNameScreen(
+    private val screenModel: OnBoardingScreenModel
+): Screen {
+
+    @Composable
+    override fun Content() {
+
+        val navigator = LocalNavigator.current
+        val state by screenModel.state.collectAsState()
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                Image(
+                    imageVector = Icons.Outlined.Check, // замени на свой ресурс
+                    contentDescription = null,
+                    contentScale = ContentScale.Fit,
+                    alignment = Alignment.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1f)
+                )
+
+                Spacer(Modifier.height(32.dp))
+
+                Text(
+                    text = "Как тебя зовут?",
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                        .fillMaxWidth(),
+                    style = MaterialTheme.typography.displaySmall,
+                )
+
+                Text(
+                    text = "Нам очень важно знать это.",
+                    modifier = Modifier
+                        .padding(bottom = 32.dp)
+                        .fillMaxWidth(),
+                    style = MaterialTheme.typography.titleMedium,
+                )
+
+                CustomTextField(
+                    modifier = Modifier
+                        .padding(bottom = 32.dp),
+                    value = state.name,
+                    onValueChange = {
+                        if (it.length <= 20) screenModel.updateName(it)
+                    },
+                    label = "Имя"
+                )
+
+                PrimaryButton(
+                    onClick = {
+                        navigator?.push(OnBoardingNameScreen(screenModel))
+                        screenModel.nextStep()
+                    },
+                    text = "Вперёд!",
+                    enabled = screenModel.isNameValid
+                )
+
+            }
+        }
+    }
+
+}
