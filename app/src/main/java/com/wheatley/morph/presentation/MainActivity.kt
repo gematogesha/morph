93330c4751101d816c99163dead22b0130f49cb9
Alan Wheatley <mr.rubikscube@mail.ru>
2025-07-21 20:34:02 +0500

    Удаление задержки и индикатора загрузки в MainActivity
    - Удалена искусственная задержка в `checkRegistrationAndNavigate`.
    - Удален `CircularProgressIndicator` и связанная с ним логика отображения состояния загрузки.
    - Приложение теперь сразу переходит к проверке регистрации и навигации без отображения экрана загрузки.

--- app/src/main/java/com/wheatley/morph/presentation/MainActivity.kt
+++ app/src/main/java/com/wheatley/morph/presentation/MainActivity.kt
@@ -5,23 +5,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.wheatley.morph.data.local.prefs.User
 import com.wheatley.morph.presentation.onboarding.OnBoardingActivity
-import com.wheatley.morph.ui.theme.MorphTheme
 import com.wheatley.morph.ui.theme.ThemeManager
-import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,32 +26,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            var isLoading by remember { mutableStateOf(true) }
-
-            MorphTheme {
-                Surface {
-                    if (isLoading) {
-                        Box(
-                            modifier = Modifier.fillMaxSize(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            CircularProgressIndicator()
-                        }
-                    }
-                }
-            }
-
             LaunchedEffect(Unit) {
                 checkRegistrationAndNavigate()
-                isLoading = false
                 keepSplashOnScreen = false
             }
         }
     }
 
     private suspend fun checkRegistrationAndNavigate() {
-        delay(1000)
-
         val isRegistered = User.isRegistered(applicationContext)
         val targetActivity = if (isRegistered) DashboardActivity::class.java else OnBoardingActivity::class.java
 
