21b53e9de467b9dca60f89ff21a446539ff08156
Ivan <mr.rubikscube@mail.ru>
2025-07-25 14:32:02 +0500

    Обновление KSP до 2.2.0-2.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.devtools.ksp") version "2.1.21-2.0.1"
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
     kotlin("plugin.serialization") version "1.9.0"
 }
