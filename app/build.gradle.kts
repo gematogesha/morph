d66731be7216065766fdc1d463cde9f124c2fbcf
Alan Wheatley <mr.rubikscube@mail.ru>
2025-08-23 02:29:23 +0500

    Обновление версии приложения
    - `versionName` изменено с "0.7.0" на "0.7.1".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 4
-        versionName = "0.7.0"
+        versionName = "0.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
